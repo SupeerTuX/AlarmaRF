6dc3bd8c5999a59454e4e1e4cd8e874f11c81974
Tux <tuxfamily.one@gmail.com>
2019-09-03 18:20:27 -0500

    Base multireceptor
    La base es capaz de recibir mensajes de varias direcciones (hasta 6)

--- AlarmaRF/NRFEmisor/src/main.cpp
+++ AlarmaRF/NRFEmisor/src/main.cpp
@@ -1,54 +1,41 @@
-#include <SPI.h>
-#include <nRF24L01.h>
+/*
+  Arduino Wireless Network - Multiple NRF24L01 Tutorial
+        == Node 02 (Child of Master node 00) ==    
+*/
+#include <RF24Network.h>
 #include <RF24.h>
+#include <SPI.h>
 
 //Declaremos los pines CE y el CSN
 #define CE_PIN 10
 #define CSN_PIN 9
- 
-//Variable con la dirección del canal por donde se va a transmitir
-byte direccion[5] ={'c','a','n','a','l'};
 
 //creamos el objeto radio (NRF24L01)
-RF24 radio(CE_PIN, CSN_PIN);
+RF24 radio(CE_PIN, CSN_PIN); // nRF24L01 (CE,CSN)
+RF24Network network(radio);  // Include the radio in the network
 
-//vector con los datos a enviar
-float datos[3];
-
+const uint16_t this_node = 02;       // Address of our node in Octal format ( 04,031, etc)
+const uint16_t base = 00;            // Address of the other node in Octal format
+const unsigned long interval = 1000; //ms  // How often to send data to the other unit
+unsigned long last_sent;             // When did we last send?
 void setup()
 {
-  //inicializamos el NRF24L01 
-  radio.begin();
-  radio.setPALevel(RF24_PA_MAX);
-  //inicializamos el puerto serie
-  Serial.begin(115200); 
-  Serial.println("Inicio de programa EMISOR");
- 
-   //Abrimos un canal de escritura
-  radio.openWritingPipe(direccion);
- 
+   SPI.begin();
+   radio.begin();
+   network.begin(90, this_node); //(channel, node address)
+   radio.setDataRate(RF24_1MBPS);
 }
- 
-void loop()
-{ 
- //cargamos los datos en la variable datos[]
- //datos[0]=analogRead(0)* (5.0 / 1023.0);
- //datos[1]=millis();
- //datos[2]=3.14;
 
- char datos[] = {"AT+StatusOK"};
- //enviamos los datos
- bool ok = radio.write(datos, sizeof(datos));
-  //reportamos por el puerto serial los datos enviados 
-  if(ok)
-  {
-     Serial.print("Datos enviados: "); 
-     Serial.println(datos); 
-   
-  }
-  else
-  {
-     Serial.println("no se ha podido enviar");
-  }
-  delay(1000);
+void loop()
+{
+   network.update();
+   //===== Sending =====//
+   unsigned long now = millis();
+   if (now - last_sent >= interval)
+   { // If it's time to send a data, send it!
+      last_sent = now;
+      unsigned long ledBrightness = 128;
+      RF24NetworkHeader header(base);                                         // (Address where the data is going)
+      bool ok = network.write(header, &ledBrightness, sizeof(ledBrightness)); // Send the data
+   }
 }
--- AlarmaRF/NRFReceptor/src/main.cpp
+++ AlarmaRF/NRFReceptor/src/main.cpp
@@ -1,53 +1,61 @@
+/**
+ * @file main.cpp
+ * @author SuperTux (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-08-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ * Receptor Base de la red NRF24l01 nodo 00
+ * Este modulo recibe las transmisiones de los
+ * emisores de de la red NRF24l01
+ */
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
- 
+#include <RF24Network.h>
+
 //Declaremos los pines CE y el CSN
 #define CE_PIN 10
 #define CSN_PIN 9
- 
+
 //Variable con la dirección del canal que se va a leer
-byte direccion[5] ={'c','a','n','a','l'}; 
+//byte direccion[5] = {'c', 'a', 'n', 'a', 'l'};
+const uint16_t base = 00;
 
 //creamos el objeto radio (NRF24L01)
 RF24 radio(CE_PIN, CSN_PIN);
+RF24Network network(radio);
 
 //vector para los datos recibidos
-char datos[12];
+char data[32] = {};
 
 void setup()
 {
- //inicializamos el NRF24L01 
+  //inicializamos el NRF24L01
   radio.begin();
   radio.setPALevel(RF24_PA_MAX);
-  //inicializamos el puerto serie
-  Serial.begin(115200); 
-  Serial.println("Inicio de programa RECEPTOR");
-  
-  //Abrimos el canal de Lectura
-  radio.openReadingPipe(1, direccion);
-  
-    //empezamos a escuchar por el canal
-  radio.startListening();
- 
-}
- 
-void loop() {
- //uint8_t numero_canal;
- //if ( radio.available(&numero_canal) )
- if ( radio.available() )
- {    
-     //Leemos los datos y los guardamos en la variable datos[]
-     radio.read(datos,sizeof(datos));
-     
-     //reportamos por el puerto serial los datos recibidos
-     Serial.print("Datos recividos: ");
-     Serial.println(datos);
+  network.begin(90, base);
+  radio.setDataRate(RF24_1MBPS);
 
- }
- else
- {
-     Serial.println("No hay datos de radio disponibles");
- }
- delay(1000);
+  //inicializamos el puerto serie
+  Serial.begin(115200);
+  Serial.println("Inicio de programa RECEPTOR");
+}
+
+void loop()
+{
+  network.update();
+  while (network.available())
+  {
+    RF24NetworkHeader header;
+    unsigned long incomingData;
+    network.read(header, data, sizeof(data));
+    Serial.print("Data From: ");
+    Serial.print(header.from_node);
+    Serial.print(" Data: ");
+    Serial.println(data);
+  }
 }
